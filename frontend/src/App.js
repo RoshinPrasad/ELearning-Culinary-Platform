c611c02a6f50b418213f16b717d7f3aa471fe904
unknown <roshinandalt275@gmail.com>
2023-12-27 21:48:59 +0530

    eleventh commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,36 @@
-import "./App.css";
 import React, { useState, useEffect, lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Loader from "./components/additional/Loader";
-import Mentorssignup from "./components/mentor/Mentorssignup";
-import MentorLogin from "./components/mentor/MentorLogin";
-import Onboard from "./components/mentor/Onboard";
-import Mentorsmanage from "./components/admin/Mentorsmanage";
-import About from "./components/about/About";
-import Team from "./components/team/Team";
-import Pricing from "./components/pricing/Pricing";
-import Blog from "./components/blog/Blog";
-import Contact from "./components/contact/Contact";
-import MentorDashboard from "./components/mentor/mentorDashboard";
-import DashboardComponent from "./components/mentor/DashboardComponent";
-import ClassManagementComponent from "./components/mentor/ClassManagementComponent";
-import MentorProfileComponent from "./components/mentor/MentorProfileComponent";
-import NotificationsComponent from "./components/mentor/NotificationsComponent";
-import ChatComponent from "./components/mentor/ChatComponent.jsx";
 
-// using LazyLoader for codesplitting - intial loading faster ( lazy() + <Suspense> ) */}
+const Home = lazy(() => import("./components/home/Home"));
+const About = lazy(() => import("./components/about/About"));
+const Team = lazy(() => import("./components/team/Team"));
+const Pricing = lazy(() => import("./components/pricing/Pricing"));
+const Blog = lazy(() => import("./components/blog/Blog"));
+const Contact = lazy(() => import("./components/contact/Contact"));
+const CourseHome = lazy(() => import("./components/allcourses/CourseHome"));
 
 const Userlogin = lazy(() => import("./components/Userlogin"));
 const Usersignup = lazy(() => import("./components/Usersignup"));
-const Home = lazy(() => import("./components/home/Home"));
 const UserProfile = lazy(() => import("./components/UserProfile"));
+
 const Adminlogin = lazy(() => import("./components/admin/Adminlogin"));
 const AdminHome = lazy(() => import("./components/admin/Adminhome"));
 const AdminProfile = lazy(() => import("./components/admin/AdminProfile"));
 const Create = lazy(() => import("./components/admin/Create"));
 const EditUser = lazy(() => import("./components/admin/EditUser"));
 const DeleteUser = lazy(() => import("./components/admin/DeleteUser"));
-const CourseHome = lazy(() => import("./components/allcourses/CourseHome"));
+const Mentorsmanage = lazy(() => import("./components/admin/Mentorsmanage"));
+
+const Mentorssignup = lazy(() => import("./components/mentor/Mentorssignup"));
+const MentorLogin = lazy(() => import("./components/mentor/MentorLogin"));
+const Onboard = lazy(() => import("./components/mentor/Onboard"));
+const MentorDashboard = lazy(() => import("./components/mentor/mentorDashboard"));
+const DashboardComponent = lazy(() => import("./components/mentor/DashboardComponent"));
+const ClassManagementComponent = lazy(() => import("./components/mentor/ClassManagementComponent"));
+const MentorProfileComponent = lazy(() => import("./components/mentor/MentorProfileComponent"));
+const NotificationsComponent = lazy(() => import("./components/mentor/NotificationsComponent"));
+const ChatComponent = lazy(() => import("./components/mentor/ChatComponent"));
 
 const LoaderWrapper = ({ children }) => {
   const [loading, setLoading] = useState(true);
@@ -47,12 +47,12 @@ function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Suspense>
+        <Suspense fallback={<Loader />}>
           <Routes>
             {/* user routes */}
             <Route path="/login" element={<Userlogin />} />
             <Route path="/signup" element={<Usersignup />} />
-            <Route path="/" element={<LoaderWrapper> <Home /> </LoaderWrapper> }/>
+            <Route path="/" element={<LoaderWrapper><Home /></LoaderWrapper>} />
             <Route path="/user-profile" element={<UserProfile />} />
             <Route path="/about" element={<About />} />
             <Route path="/courses" element={<CourseHome />} />
@@ -70,7 +70,7 @@ function App() {
             <Route path="/delete/:id" element={<DeleteUser />} />
             <Route path="/mentorsmanage" element={<Mentorsmanage />} />
 
-            {/* {mentor routes} */}
+            {/* mentor routes */}
             <Route path="/mentorsignup" element={<Mentorssignup />} />
             <Route path="/mentorlogin" element={<MentorLogin />} />
             <Route path="/mentoronboard" element={<Onboard />} />
